f73eea11962fa8e9430191f326f8a9734d96d447
erx <michal.rutowicz94@gmail.com>
2022-12-05 22:34:46 +0100

    DictionaryChecker complete

---
+++ src/main/java/pl/codellegance/dictionary/LinguApp.java
@@ -0,0 +1,9 @@
+package pl.codellegance.dictionary;
+
+public class LinguApp {
+
+    public static void main(String[] args) {
+        LinguController controller = new LinguController();
+        controller.mainLoop();
+    }
+}
---
+++ src/test/java/pl/codellegance/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.codellegance.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
